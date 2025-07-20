43e0fc65dd9c6c692bb2dded7622de318dbac39c
Bkrone <bradleykronep@gmail.com>
2025-07-20 09:43:04 -0500

    added electrical top bar.   Moved motor into electrical

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,6 +108,12 @@ const config = {
             position: 'left',
             label: 'Mechanical',
           },
+          {
+            type: 'docSidebar',
+            sidebarId: 'ElectricalSidebar',
+            position: 'left',
+            label: 'Electrical',
+          },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/Litemage/argos1756-docs',
