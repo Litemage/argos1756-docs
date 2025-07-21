29f17a2e0b299a9ef234508928844f175830cec0
Bkrone <bradleykronep@gmail.com>
2025-07-20 19:20:50 -0500

    added Robot section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -135,6 +135,12 @@ const config = {
             position: 'left',
             label: 'Electrical',
           },
+           {
+            type: 'docSidebar',
+            sidebarId: 'RobotSidebar',
+            position: 'left',
+            label: 'Robot',
+          },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/Litemage/argos1756-docs',
